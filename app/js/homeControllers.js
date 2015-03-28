950b3c701db6485bfd31b8369655d5c253f0ef84
orangexxx <xiayangxxx@gmail.com>
2015-03-28 12:20:27 +0800

    20150328

---
+++ app/js/homeControllers.js
@@ -0,0 +1,12 @@
+(function() {
+	var homeControllers = angular.module('homeControllers', []);
+
+	homeControllers.controller('homeListCtrl', ['$http', '$scope', function($http, $scope) {
+
+	}]);
+
+	homeControllers.controller('homeDetailCtrl', ['$http', '$scope', function($http, $scope) {
+
+	}]);
+
+})();
